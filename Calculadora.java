e83e62599474c81ff319e5e10880fbdc8b4af621
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-09-18 14:41:24 -0300

    Fix formatting in main method of Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -3,7 +3,7 @@ public class Calculadora {
         return a + b;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         Calculadora calc = new Calculadora();
         System.out.println("Resultado: " + calc.somar(2, 3));
     }

7abddfaa812b8797b63184ebf29815c6d1d99b97
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2026-09-18 14:49:52 -0300

    Fix syntax error in Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,9 @@
 public class Calculadora {
     public int somar(int a, int b) {
-        return a + b;
+        return a + b
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         Calculadora calc = new Calculadora();
         System.out.println("Resultado: " + calc.somar(2, 3));
     }
